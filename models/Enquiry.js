10615026f0bfeda70896552ca4f8148099af8074
Casper Ndlovu <casperndlovu42@gmail.com>
2018-02-03 08:41:31 +0200

    Fixed spelling errors. Fixed email functionality

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -35,8 +35,70 @@ Enquiry.schema.post('save', function () {
 	}
 });
 
-Enquiry.schema.methods.sendNotificationEmail = function (callback) {
-	if (typeof callback !== 'function') {
+Enquiry.schema.methods.sendNotificationEmail = function (callback)
+{
+	var enquiry = this;
+	var mailjet = require('node-mailjet').connect('f8d3d1d74c95250bb2119063b3697082','8304b30da4245632c878bf48f1d65d92');
+	//send email notification to stakeholders
+	var request = mailjet.post("send").request(
+	{
+		"FromName": enquiry.name.first+" "+enquiry.name.last,
+		"FromEmail": "system@pioneerdesigns.co.za",
+		"Subject": "New Enquiry From " + enquiry.name.first + " " + enquiry.name.last,
+		//"Text-part": enquiry.name.first + " " + enquiry.name.last +" says,\n" + enquiry.message.html,
+		"Html-part": "<p style='font-size:22px;'>"+enquiry.name.first+" "+enquiry.name.last+" left a message of type <strong>"+enquiry.enquiryType +"</strong>,<br/><br/></p>"
+					+ "<h4>Phone:<i>" + enquiry.phone + "</i></h4><br/>"
+					+ "<h4>e-Mail:<i>" + enquiry.email + "</i></h4><br/>"
+					+ "<h2>Message:</h2><br/><p>" + enquiry.message.html + "</p><br/><br/>"+"Kindest Regards,<br/>Factoryshop Mailing System.",
+		"Recipients":
+		[
+			{
+				"Email": "enquiries@factoryshopza.co.za"
+			}
+		]
+		}, function(err, res)
+		{
+			if(err)
+			{
+				console.log('email send error: %s', err);
+				if(callback)
+					callback(err);
+				return;
+			}
+			console.log('email sent to system email.');
+			//send email to user/client as well.
+			var msg = "<p>Hi " + enquiry.name.first+",<br/>We've received your enquiry about " + enquiry.enquiryType
+						" and one of our consultants will get back to you soon.</p><br/><br/>";
+				msg += "<h3>Your message was:</h3><br/><i>" + enquiry.message.html + "</i><br/><br/>";
+				msg += "<h3>Thank you!</h3><br/><br/>Kindest Regards,<br/>Factoryshop Team.";
+			request = mailjet.post("send").request(
+			{
+				"FromName": "Factoryshop",
+				"FromEmail": "system@pioneerdesigns.co.za",
+				"Subject": "Enquiry Receipt",
+				//"Text-part": msg,
+				"Html-part": msg,
+				"Recipients":
+				[
+					{
+						"Email": enquiry.email
+					}
+				]
+			}, function(err, res)
+			{
+				if(err)
+				{
+					console.log('could not send email to [%s]: %s', enquiry.email, err);
+				}else
+				{
+					console.log('email successfully sent to [%s] ', enquiry.email);
+				}
+				if(callback)
+					callback(err);
+			});
+		}
+	);
+	/*if (typeof callback !== 'function') {
 		callback = function (err) {
 			if (err) {
 				console.error('There was an error sending the notification email:', err);
@@ -67,7 +129,7 @@ Enquiry.schema.methods.sendNotificationEmail = function (callback) {
 			enquiry: enquiry,
 			brand: brand,
 		}, callback);
-	});
+	});*/
 };
 
 Enquiry.defaultSort = '-createdAt';
